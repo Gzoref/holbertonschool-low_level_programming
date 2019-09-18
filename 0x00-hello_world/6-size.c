e06590a018481ac13d12e58134ec9f6ee23032c8
Gzoref <gzoref@gmail.com>
2019-09-18 13:27:18 -0400

    Prints sizes of variables

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - Prints the size of each variable
+* Return: 0 prints sizes
+*/
+int main(void)
+{
+	printf("Size of a char: %lu\n", sizeof(char));
+	printf("Size of an int: %lu\n", sizeof(int));
+	printf("Size of a long int: %lu\n", sizeof(long));
+	printf("Size of a long long int: %lu\n", sizeof(long long int));
+	printf("Size of a float: %lu", sizeof(float));
+        return (0);
+}
