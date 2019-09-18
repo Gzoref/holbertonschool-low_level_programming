9ebb12cd208c64b43287b537a67099fe18329a07
Gzoref <gzoref@gmail.com>
2019-09-18 13:00:50 -0400

    Output to console using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - Prints using puts() command
+* Return: 0 prints something
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
