48ce3c136afd7cb8237847cb5b06d4165b783415
Gzoref <gzoref@gmail.com>
2019-09-19 14:36:02 -0400

    1-9 with commas

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - A program that prints all combinations of single digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar(num + '0');
+		if (num != 9)
+		{
+		putchar(',');
+		putchar(' ');
+		}
+		}
+	putchar('\n');
+		return (0);
+}
