ed5e0877119b78aa38c9fb46f5d93e379c209c41
Gzoref <gzoref@gmail.com>
2019-09-19 13:44:32 -0400

    Less than or greater

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,14 +14,6 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if (n < 0)
-	{
-		lastDigit = n * -1 % 10;
-	}
-	else
-	{
-		lastDigit = n % 10;
-	}
 	if (lastDigit > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
