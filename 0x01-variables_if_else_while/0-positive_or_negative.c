0c4c5ce045a82021d37f269bdf569ea7c457ab86
Gzoref <gzoref@gmail.com>
2019-09-19 12:40:56 -0400

    Less than or greater than 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,15 +14,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("%i is positive", n);
+		printf("%i is positive\n", n);
 	}
 	else if (n < 0)
 	{
-		printf("%i is negative", n);
+		printf("%i is negative\n", n);
 	}
 	else
 	{
-		printf("%i is zero");
+		printf("%i is zero\n");
 	}
 	return (0);
 }
