eea17de94b9c36f12fc78cf76e95d86c32768915
Gzoref <gzoref@gmail.com>
2019-09-19 14:00:44 -0400

    base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -13,7 +13,9 @@ int main(void)
 	for (num = 0; num <= 9; num++)
 		putchar(num + '0');
 	for (character = 'a'; character <= 'f'; character++)
+	{
 		putchar(character);
-
+	}
+	putchar('\n');
 	return (0);
 }
