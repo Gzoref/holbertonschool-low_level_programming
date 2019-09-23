ea93264c463952afe1bbb1d6347000d70f2d085a
Gzoref <gzoref@gmail.com>
2019-09-23 15:33:23 -0400

    Holberton print

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "holberton.h"
 /**
  * main - Write a program that prints Holberton, followed by a new*
  *
@@ -7,7 +7,16 @@
  */
 int main(void)
 {
-	printf("Holberton\n");
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
 
 	return (0);
 }
