fa0a3810709668938210780ddf4d440cf47e6efa
Gzoref <gzoref@gmail.com>
2019-09-23 15:39:11 -0400

    Alphabet 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - prints 10 times the alphabet
+ * * Return: On success 1.
+ */
+void print_alphabet_x10(void)
+{
+	int letter, counter = 0;
+
+	while(counter < 10)
+	{
+		for(letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+		counter++;
+		_putchar('\n');
+	}
+}
