6621df19b7dc57dbde476666a36bc95caaa6172f
Gzoref <gzoref@gmail.com>
2019-09-24 18:41:01 -0400

    Largest int

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,20 +10,20 @@
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
+	int largest;
 
-    if (a > b || b > c)
-    {
-        largest = a;
-    }
-    else if (b > a || a > c)
-    {
-        largest = b;
-    }
-    else
-    {
-        largest = c;
-    }
+	if (a > b || b > c)
+	{
+		largest = a;
+	}
+	else if (b > a || a > c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
 
-    return (largest);
+	return (largest);
 }
