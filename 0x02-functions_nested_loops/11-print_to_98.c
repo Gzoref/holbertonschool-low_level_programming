ad9358c63f37710e571fb4e4c742f9ead4494fe2
Gzoref <gzoref@gmail.com>
2019-09-24 14:30:39 -0400

    Print n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -26,11 +26,11 @@ int print_to_98(int n)
 	{
 		for (; n >= 98; n--)
 		{
-			printf("%d ", n);
+			printf("%d", n);
 
 			if (n != 98)
 			{
-				putchar(',');
+				printf(", ");
 			}
 		}
 		printf("\n");
