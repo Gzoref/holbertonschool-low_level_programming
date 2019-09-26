08083877fd4d4cfae82e44e5c739a9dd55fbd248
root <root@DESKTOP-6P5KUTR.localdomain>
2019-09-26 17:04:46 -0400

    a x b

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * mul - function that multiplies two integers.
+ *
+ * @a: first operand
+ * 
+ * @b: second operand
+ *
+ Return: multiplication of a and b
+ */
+
+int mul(int a, int b)
+{
+  return (a * b);
+}
