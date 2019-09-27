16325d336a4ff3eec5b65c02ef0307a22c99f04d
Gzoref <gzoref@gmail.com>
2019-09-26 21:18:10 -0400

    Print 0-14 ten times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,27 +1,26 @@
 #include "holberton.h"
 
 /**
- * more_numbers - prints 10 times the numbers, from 0 to 14,
+ * more_numbers - prints 10 times the numbers, from 0 to 14
  *
- * Return: void
+ *
  */
 
 void more_numbers(void)
 {
-	int i, k;
+	int i;
+	int count;
 
-	for (k = 0; k <= 10; k++)
+	for (count = 0; count < 10; count++)
 	{
+		for (i = 0; i <= 14; i++)
 		{
-			for (i = 0; i <= 14; i++)
+			if (i > 9)
 			{
-				if (i > 9)
-				{
-					_putchar(i / 10 + '0');
-				}
-				_putchar(i % 10 + '0');
+				_putchar(i / 10 + '0');
 			}
-			_putchar('\n');
+			_putchar (i % 10 + '0');
 		}
+		_putchar ('\n');
 	}
 }
