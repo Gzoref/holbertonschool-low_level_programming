81dfa8326ad9ace5118fdd47f12b2676dcc52b6f
Gzoref <gzoref@gmail.com>
2019-09-26 20:23:19 -0400

    Print 0-9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,14 +3,14 @@
 /**
  * print_numbers - Prints the numbers, from 0 to 9
  *
- * Returnvoid
+ * Return: void
  */
 
 void print_numbers(void)
 {
 	int i;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i <= 9 ; i++)
 	{
 		_putchar(i + '0');
 	}
