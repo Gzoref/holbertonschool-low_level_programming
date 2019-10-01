5edc450ede05cbfd91602a93307228dfb0d5c905
Gzoref <gzoref@google.com>
2019-10-01 13:36:05 -0400

    Print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * print_array -  prints arrays
+ *
+ * @a: arg a
+ * @n: num
+ *
+ * Return: string to stdout
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++) {
+		if(i != n - 1)
+		{
+			printf("%d, ", a[i]);
+		}
+		else
+		{
+			printf("%d", a[i]);
+		}
+	}
+	putchar('\n');
+}
