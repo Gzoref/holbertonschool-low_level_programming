b6dfe57babb15bbf9ecf8b89b88c4ec8339c8cd6
Gzoref <gzoref@google.com>
2019-10-01 12:56:24 -0400

    Print half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,37 +6,37 @@
   *
   * Return: string to stdout
   */
+
 void puts_half(char *str)
 {
-	 int length, n;
+	int length, n;
 
-	 length = 0;
+	length = 0;
 
-	 while (str[length] != '\0')
-	 {
-		 length++;
+	while (str[length] != '\0')
+	{
+		length++;
+	}
 
-	 }
+	if (length % 2 == 0)
+	{
+		n = length / 2;
 
-	 if (length % 2 == 0)
-	 {
-		 n = length / 2;
+		while (length - 1 >= n)
+		{
+			_putchar(str[n]);
+			n++;
+		}
+	}
+	else
+	{
+		n = (length - 1) / 2;
 
-		 while(n < length)
-		 {
-			 _putchar(str[n]);
-			 n++;
-		 }
-	 }
-	 else
-	 {
-		 n = (length - 1) / 2;
-
-		 while(length >= n)
-		 {
-			 _putchar(str[n]);
-			 n++;
-		 }
-	 }
-	 _putchar('\n');
+		while (length - 1 >= n)
+		{
+			_putchar(str[n]);
+			n++;
+		}
+	}
+	_putchar('\n');
 }
