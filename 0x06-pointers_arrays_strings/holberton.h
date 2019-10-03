c140fa30076b78f1091d2fab8d686a48955546a4
Gzoref <gzoref@gmail.com>
2019-10-03 16:43:53 -0400

    Headers

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,2 +1,16 @@
-char *_strcat(char *dest, char *src);
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 int _putchar(char c);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+int _strlen(char *s);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+#endif
