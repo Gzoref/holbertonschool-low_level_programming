8c2d172471b29f2390bff5ad89c68da98029297b
Gzoref <gzoref@google.com>
2019-10-03 13:59:02 -0400

    Rot13

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * rot13 - caesers cipher
+ * @str: pointer to an array of words
+ *
+ * Return: s
+ */
+
+char *rot13(char *str)
+{
+	int i, j;
+	char input[80] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char output[80] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; str[i] != '\0'; ++i)
+	{
+		for (j = 0; input[j] != '\0'; j++)
+		{
+			if (str[i] == input[j])
+			{
+				str[i] = output[j];
+				break;
+			}
+		}
+
+	}
+	return (str);
+}
