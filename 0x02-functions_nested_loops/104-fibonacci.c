2b41634a4be9f5953e18b1af7ef42a3adaa927c7
Gzoref <gzoref@gmail.com>
2019-10-06 17:01:43 -0400

    Fib 98

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Prints the first 98 Fibonacci numbers
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int index;
+	long int remainder1, remainder2, fib1, fib2, div1, div2, quot;
+
+	fib1 = 0;
+	fib2 = 1;
+	div1 = 0;
+	div2 = 0;
+	remainder1 = 0;
+	remainder2 = 2;
+
+	printf("%ld, %ld, ", fib1, fib2);
+
+	for (index = 2; index < 98; index++)
+	{
+		quot = (fib1 + fib2) / 100000000000;
+		div1 = fib1 + fib2 + quot;
+		fib1 = remainder1;
+		remainder1 = div1;
+		fib2 = remainder2;
+		remainder2 = div2;
+	}
+	if (div1 > 0)
+	{
+		printf("%ld", div1);
+
+		if(index == 97)
+			printf("%ld\n", div2);
+		else
+			printf("ld, ", div2);
+	}
+	else
+		printf("%ld, ", div2);
+
+	return (0);
+}
