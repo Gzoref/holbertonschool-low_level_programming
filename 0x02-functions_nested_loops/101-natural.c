fd8944661a4517b9d4ea0b23ff0c8f3b201755c2
Gzoref <gzoref@gmail.com>
2019-10-06 15:59:38 -0400

    Print sum of multiples of 3, 5 to 1024

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Program to sum integers
+ *
+ * Description: check for all natural numbers in range of
+ * 3 and 5 and sum them
+ * Return: 0
+ */
+
+int main (void)
+{
+
+	int i, sum;
+
+	for(i = 0; i < 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			sum += i;
+		}
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
