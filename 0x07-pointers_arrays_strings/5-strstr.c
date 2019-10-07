002e9284bdeda65d587e59805bb501f147151ff3
Gzoref <gzoref@gmail.com>
2019-10-07 17:42:21 -0400

    Locates a substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -6,23 +6,19 @@
  * @needle: char pointer
  * @haystack: char pointer
  *
- *
  * Return: s
  */
 
 char *_strstr(char *haystack, char *needle)
 {
-	unsigned int count;
-	for (count = 0; needle[count] && haystack[count]; count++)
+	int count;
 
-		for (; needle[count] != haystack[count]; haystack++)
-			count = 0;
-		if(needle[count])
-
-			return (0);
-
-		else
-			return (char *)haystack;
-
-
+	for (; haystack[0]; haystack++)
+	{
+		for (count = 0; haystack[count] == needle[count]; count++)
+			;
+		if (!(needle[count]))
+			return (haystack);
+	}
+	return (0);
 }
