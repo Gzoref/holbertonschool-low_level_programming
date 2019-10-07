047674403f59f5e67f5ba2c3a3029cd458e674f6
Gzoref <gzoref@google.com>
2019-10-07 13:56:19 -0400

    Header

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -1,5 +1,6 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+int _putchar(char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_memset(char *s, char b, unsigned int n);
 char *_strchr(char *s, char c);
