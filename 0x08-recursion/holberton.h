e8331a13fd87e507b33183cf2706afa77fde0c59
Gzoref <gzoref@google.com>
2019-10-09 09:34:46 -0400

    Header

---
+++ 0x08-recursion/holberton.h
@@ -0,0 +1,14 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+
+#endif
