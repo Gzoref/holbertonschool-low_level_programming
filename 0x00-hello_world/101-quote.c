4a23459797096bd78bbc1e253c4ac985bef01613
Gzoref <gzoref@google.com>
2019-10-10 13:02:23 -0400

    Prints quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include "stdio.h"
+#include "string.h"
+#include "unistd.h"
+
+/**
+ * main - Main program
+ *
+ * Return: 1
+ */
+
+
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+	return (1);
+}
