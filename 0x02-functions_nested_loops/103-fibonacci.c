8b70a1faf8ed261623c78024668be58258618dc6
Gzoref <gzoref@google.com>
2019-10-10 17:16:37 -0400

    Fib to 4000000

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -3,7 +3,6 @@
 /**
  * main - Sum of the even-valued terms of fib
  *
- * Description: fibobnachi starting from one to 40000000
  *
  * Return: 0
  */
@@ -12,19 +11,22 @@ int main(void)
 {
 	long int fib1, fib2, fib3, sum;
 
-	fib1 = 1;
-	fib2 = 2;
+	fib1 = 0;
+	fib2 = 0;
+	fib3 = 1;
 	sum = 0;
 
 	while (fib2 < 4000000)
 	{
+		fib2 = fib1 + fib3;
+		fib1 = fib3;
+		fib3 = fib2;
+
 		if (fib1 % 2 == 0)
 		{
-			sum += fib1;
+			sum += fib2;
 		}
-		fib3 = fib2;
-		fib2 = fib1 + fib2;
-		fib1 = fib3;
+
 	}
 	printf("%ld\n", sum);
 
