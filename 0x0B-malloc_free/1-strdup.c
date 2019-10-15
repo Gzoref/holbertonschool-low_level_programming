d70e0455e06ad605434634109b942b1f0aa7e895
Gzoref <gzoref@google.com>
2019-10-15 10:27:47 -0400

    Returns a pointer to a newly allocated space in memory

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * _strdup - Print name of program
+ *
+ * @str: unsigned int
+ *
+ *
+ * Return: char pointer
+ */
+
+char *_strdup(char *str)
+{
+	unsigned int counter1, counter2;
+	char *strCopy;
+
+	counter1 = 0;
+	counter2 = 0;
+
+	if (str == NULL)
+	{
+		return (0);
+	}
+
+	while (str[counter1] != '\0')
+	{
+		counter1++;
+	}
+
+	strCopy = malloc(counter1 * sizeof(char));
+
+	while (counter2 < counter1)
+	{
+		strCopy[counter2] = str[counter2];
+		counter2++;
+	}
+
+	return (strCopy);
+}
