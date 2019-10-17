bdc4f01b34872d33a37bb68cebd07944388fffbf
Gzoref <gzoref@google.com>
2019-10-17 13:55:56 -0400

    Realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -33,6 +33,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (ptr == NULL)
 	{
 		str = malloc(new_size);
+		free(ptr);
+		return (str);
 	}
 
 	if (new_size == 0 && ptr != NULL)
