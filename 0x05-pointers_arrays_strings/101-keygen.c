70fc88c1924f33e05602b21d1610d5c95bca33e7
Gzoref <gzoref@google.com>
2019-10-20 17:46:02 -0400

    Random password generator

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -14,23 +14,39 @@ int main(void)
 {
 	int counter, sum, random;
 
-	char numAndLet [80] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	sum = 0;
+	counter = 0;
+	random = 0;
+
 	char password [80];
 
 	srand(time(NULL));
-	sum = 0;
-	counter = 0;
 
-	while (sum < (2772 - 122))
+	while (sum < 2772)
 	{
-		random = rand() % 62;
-		password[counter] = numAndLet[random];
-		sum += password[counter];
-		counter++;
+
+		if (2772 - sum < 48)
+	{
+		sum -= password[--counter];
+	}
+		else if (2772 - sum <= 126)
+		{
+			random = 2772 - sum;
+		}
+		else
+		{
+			random = rand() % (126 - 48) + 48;
+		}
+
+		if (random)
+		{
+			password[counter++] = random;
+			sum += random;
+		}
+		random = 0;
 	}
 
-	random = 2772 - sum;
-	password[counter] = random;
+	password[counter] = '\0';
 	printf("%s\n", password);
 
 	return 0;
