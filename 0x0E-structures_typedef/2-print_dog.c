10560cbf77a8478081a8f3a9d74cdaacf42f4774
Gzoref <gzoref@google.com>
2019-10-21 13:08:22 -0400

    Print dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,7 +16,11 @@ void print_dog(struct dog *d)
 	}
 	else if (d->name == NULL)
 	{
-		printf("nil");
+		printf("Name: (nil)");
+	}
+	else if (d->owner == NULL)
+	{
+		printf("Owner: (nil)");
 	}
 	else
 	{
