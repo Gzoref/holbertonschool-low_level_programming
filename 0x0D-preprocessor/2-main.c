47b7b52339cca39fedae23a2dca1ff0d854c1f48
Gzoref <gzoref@google.com>
2019-10-21 09:31:46 -0400

    prints the name of the file it was compiled from, followed by a new line.

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the file it was compiled from
+ *
+ *
+ * Return: 0
+ */
+
+
+int main(void)
+{
+
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
