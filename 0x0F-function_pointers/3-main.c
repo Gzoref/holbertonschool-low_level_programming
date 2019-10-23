12dd8913f215b61123b7a4136aac43b91e21b364
Gzoref <gzoref@gmail.com>
2019-10-23 15:54:50 -0400

    Calculator

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -14,15 +14,11 @@
 
 int main(int argc, char **argv)
 {
-
-
 	if (argc == 4)
 	{
-
 		printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
 		return (0);
 	}
-
 	printf("Error\n");
-	exit(98);
+	return (98);
 }
