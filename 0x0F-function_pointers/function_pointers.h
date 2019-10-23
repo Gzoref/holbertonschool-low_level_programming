30fbb1b9115d9d3fb2fe02c1d8a0489ea8116e46
Gzoref <gzoref@gmail.com>
2019-10-23 00:54:30 -0400

    Header

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,9 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 
+#include <stdlib.h>
+
+int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
