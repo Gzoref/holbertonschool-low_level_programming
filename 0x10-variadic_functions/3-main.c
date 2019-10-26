1e9597846b40cccb706ecf9e2dc93efcd2abdb53
Gzoref <gzoref@gmail.com>
2019-10-26 15:23:41 -0400

    Fixes

---
+++ 0x10-variadic_functions/3-main.c
@@ -0,0 +1,12 @@
+#include "variadic_functions.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_all("cfisf", 'H', 0, "lberton", 3.14);
+	return (0);
+}
