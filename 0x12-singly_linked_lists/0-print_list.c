72b6eda4df8151bca2f1c194274756f291929fa4
Gzoref <gzoref@gmail.com>
2019-10-31 10:33:25 -0400

    Print linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+* print_list - Prints linked list
+*
+* @h - Points to struct
+*
+* Return: Integer
+*/
+
+size_t print_list(const list_t *h)
+{
+	unsigned int index = 0;
+	const list_t *list  = NULL;
+
+	list = h;
+	while (list != NULL)
+	{
+		if (list->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+			printf("[%d] %s\n", list->len, list->str);
+		list = list->next;
+		index++;
+	}
+	return (index);
+}
