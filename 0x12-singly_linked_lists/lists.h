62def37851c8b9b21a64becd6ccb386511fadcf0
Gzoref <gzoref@gmail.com>
2019-10-31 15:07:45 -0400

    Header

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -28,5 +28,6 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+void first(void) __attribute__ ((constructor));
 
 #endif
