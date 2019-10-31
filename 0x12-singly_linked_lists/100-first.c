d3a2d4125ca17bd449f1225f99c8149661f3fdcc
Gzoref <gzoref@gmail.com>
2019-10-31 15:07:31 -0400

    Print first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+* first - Print before main prints
+*
+* Return: void
+*/
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
+}
