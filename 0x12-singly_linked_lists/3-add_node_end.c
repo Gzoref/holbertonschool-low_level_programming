c263205c207e30d048014df32652a613115fdddb
Gzoref <gzoref@gmail.com>
2019-10-31 12:36:18 -0400

    Add node to end

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,61 @@
+#include "lists.h"
+
+/**
+* _strlen - Adds node to end of linked list
+*
+* @str: Pointer to struct pointer
+*
+* Return: Length of string
+*/
+
+int _strlen(const char *str)
+{
+	unsigned int length = 0;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	return (length);
+}
+
+/**
+* add_node - Adds node to end of linked list
+*
+* @head - Double Pointer
+*
+* @str: Pointer to struct pointer
+*
+* Return: address of the new element, or NULL
+*/
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *list, *end_node;
+
+	end_node = malloc(sizeof(list_t));
+
+	if (end_node == NULL)
+	{
+		return (NULL);
+	}
+	end_node->str = strdup(str);
+	end_node->len = _strlen(str);
+	end_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = end_node;
+	}
+	else
+	{
+		list = *head;
+
+		while (list->next)
+		{
+			list = list->next;
+		}
+		list->next = end_node;
+	}
+	return (end_node);
+}
