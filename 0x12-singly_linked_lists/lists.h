0bd2908a03fe666cef0380b698f9a8b6ef6475de
Gzoref <gzoref@gmail.com>
2019-10-31 12:35:07 -0400

    Header

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -22,6 +22,7 @@ typedef struct list_s
 } list_t;
 
 int _putchar(char c);
+int _strlen(const char *str);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
