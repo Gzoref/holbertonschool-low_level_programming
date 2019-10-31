c21841fcdc0b2543d63db20ba148dbb897922dda
Gzoref <gzoref@gmail.com>
2019-10-31 11:31:26 -0400

    Add new node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+* add_node - Adds node to linked list
+*
+* @head - Double Pointer
+*
+* @str: Pointer to struct pointer
+*
+* Return: address of the new element, or NULL
+*/
+
+list_t *add_node(list_t **head, const char *str)
+{
+	unsigned int index = 0;
+	list_t *list = NULL;
+
+	list = malloc(sizeof(list_t));
+
+	if (list == NULL)
+	{
+		return (NULL);
+	}
+
+	while (str[index] != '\0')
+	{
+		index++;
+	}
+	list->len = index;
+	list->str = strdup(str);
+	list->next = *head;
+	*head = list;
+	return (*head);
+}
