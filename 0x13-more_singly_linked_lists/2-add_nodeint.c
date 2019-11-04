bb82e87122473db7f42656d7af606868e4579bd2
Gzoref <gzoref@gmail.com>
2019-11-04 10:09:14 -0500

    adds a new node at the beginning

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ *  add_nodeint - Adds a new node at the beginning
+ *
+ *  @head: struct
+ *
+ * @n: const int
+ *
+ *  Return: listint_t
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node;
+
+	new_node = malloc(sizeof(listint_t));
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+	return (*head);
+}
