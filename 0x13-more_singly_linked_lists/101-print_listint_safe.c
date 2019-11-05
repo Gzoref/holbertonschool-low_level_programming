c78345fdfe5b67ff361ae71a1c8de9b79175acc3
Gzoref <gzoref@gmail.com>
2019-11-04 19:30:26 -0500

    Print listint_t

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+* print_listint_safe - Prints a listint_t linked list
+*
+* @head: listint_t head
+*
+* Return: size_t
+*/
+
+
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t count1 = 0, count2;
+	const listint_t *temp = head;
+	const listint_t *next_node;
+
+	while (temp != NULL)
+	{
+		printf("[%p] %d\n", (void *)temp, temp->n);
+
+		count1++;
+		temp = temp->next;
+		next_node = head;
+		count2 = 0;
+		while (count2 < count1)
+		{
+			if (temp == next_node)
+			{
+				printf("-> [%p] %d\n", (void *)temp->next, temp->next->n);
+				return (count1);
+			}
+			next_node = next_node->next;
+			count2++;
+		}
+	}
+	return (count1);
+}
