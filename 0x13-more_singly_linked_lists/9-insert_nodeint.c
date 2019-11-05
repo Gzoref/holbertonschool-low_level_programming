196dc8d40700c858cbd4642972fedfbbc369f9f9
Gzoref <gzoref@gmail.com>
2019-11-05 13:08:12 -0500

    Add new node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,29 +14,39 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node = malloc(sizeof(listint_t));
+	unsigned int count = 0;
+	listint_t *current_node, *new_node = malloc(sizeof(listint_t));
+
+	if (head == NULL || new_node == NULL)
+	{
+		return (NULL);
+	}
 
 	new_node->n = n;
+	new_node->next = NULL;
 
-	if (head == NULL)
-	{
-		return (new_node);
-	}
-
-	if (idx == 0)
+	if (!idx)
 	{
 		new_node->next = *head;
+		*head = new_node;
 		return (new_node);
 	}
-	listint_t *current_node = *head;
 
-	while (idx - 1 > 0)
+	current_node = *head;
+
+	while (current_node)
 	{
-		current_node = current_node->next;
-		idx--;
-	}
-	new_node->next = current_node->next;
-	current_node->next = new_node;
 
-	return (*head);
+		if (count == (idx - 1))
+		{
+			new_node->next = current_node->next;
+			current_node->next = new_node;
+			return (new_node);
+		}
+		count++;
+		current_node = current_node->next;
+	}
+	free(new_node);
+
+	return (NULL);
 }
