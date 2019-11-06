e8ae4f9ff665a0d6d193cca056aaf09800ddb3b2
Gzoref <gzoref@gmail.com>
2019-11-06 17:52:29 -0500

    Free listint safe

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -11,11 +11,28 @@
 size_t free_listint_safe(listint_t **h)
 {
 	size_t count = 0;
+	int count2;
 	listint_t *head, *new_node;
-	
+
 	if (*h == NULL)
 	{
 		return (0);
+	}
+
+	head = *h;
+
+	while (head)
+	{
+		count++;
+		count2 = head->next - head;
+		new_node = head;
+		free(new_node);
+
+		if (count2 >= 0)
+		{
+			break;
+		}
+		head = head->next;
 	}
 	return (count);
 }
