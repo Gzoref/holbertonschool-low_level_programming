9e17a98523975ee95145f1aea62989e245ffe137
Gzoref <gzorf@gmail.com>
2019-11-07 13:35:14 -0500

    Binary to unsigned int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,61 @@
+#include "holberton.h"
+
+/**
+ * binary_to_uint - Converts binary to unsigned int
+ *
+ * @b: Pointer to string of chars
+ *
+ * Return: Converted number
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int index = 0;
+	unsigned int number = 0, number1, decimal_value = 0;
+
+	if (b == NULL)
+	{
+		return (0);
+	}
+
+	while (b[index])
+	{
+		if (b[index] != '1' && b[index] != '0')
+		{
+			return (0);
+		}
+		index++;
+	}
+
+	index = 0;
+	number = _atoi(b);
+
+	while (number != 0)
+	{
+		number1 = number % 10;
+		decimal_value += number1 << index;
+		number /=  10;
+		index++;
+	}
+	return (decimal_value);
+}
+
+
+/**
+ * _atoi - Converts Ascii to integer
+ *
+ * @str: Pointer to string of chars
+ *
+ * Return: Converted number
+ */
+
+unsigned int _atoi(const char *str)
+{
+	unsigned int index = 0, result = 0;
+
+	for (index = 0; str[index] != '\0'; index++)
+	{
+		result = result * 10 + str[index] - '0';
+	}
+	return (result);
+}
