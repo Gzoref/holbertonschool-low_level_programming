73871d624772149e62391c9db06d0774758eed76
Gzoref <gzorf@gmail.com>
2019-11-07 13:34:47 -0500

    Header

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,7 +1,11 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 int _putchar(char c);
+unsigned int _atoi(const char *str);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
