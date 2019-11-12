b461f635a999e8dbd7e22cfc1ada73866c9faa24
Gzoref <gzoref@gmail.com>
2019-11-12 14:35:09 -0500

    Create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+
+/**
+ * create_file - Creates a new file
+ *
+ * @filename: Param description
+ *
+ * @text_content: Param description
+ *
+ * Return: int
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fp;
+	ssize_t length = 0, bytes = 0;
+
+	while (text_content[length] != '\0')
+	{
+		length++;
+	}
+
+	fp = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fp == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content == NULL)
+	{
+		text_content = "";
+	}
+
+	bytes = write(fp, text_content, length);
+
+	if (bytes == -1)
+	{
+		return (-1);
+	}
+	close(fp);
+
+	return (1);
+}
