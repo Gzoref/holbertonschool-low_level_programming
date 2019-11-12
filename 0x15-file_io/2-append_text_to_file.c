aac634020c4ac8dbb909c4cb64e7cafce47b5eb8
Gzoref <gzoref@gmail.com>
2019-11-12 18:15:51 -0500

    Append to file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,25 +21,24 @@ int append_text_to_file(const char *filename, char *text_content)
 		length++;
 	}
 
-	if (filename == NULL)
-	{
+	if (!filename)
 		return (-1);
-	}
-
 	fp = open(filename, O_WRONLY | O_APPEND);
-
 	if (fp == -1)
 	{
 		return (-1);
 	}
+		if (length)
+		{
+			bytes = write(fp, text_content, length);
+		}
+		close(fp);
+		if (bytes == -1)
+		{
+			return (-1);
+		}
 
-	bytes = write(fp, text_content, length);
+		close(fp);
 
-	if (bytes == -1)
-	{
-		return (-1);
-	}
-	close(fp);
-
-	return (1);
+		return (1);
 }
