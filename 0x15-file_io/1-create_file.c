0d5867b4ba8d935fe0f0577c60bd750679b54311
Gzoref <gzoref@gmail.com>
2019-11-12 16:19:25 -0500

    Create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -29,9 +29,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content == NULL)
 	{
-		text_content = "";
+		return (1);
 	}
-
 	bytes = write(fp, text_content, length);
 
 	if (bytes == -1)
