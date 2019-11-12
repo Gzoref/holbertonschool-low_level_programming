dcb040e3f5dab82ce03cc17c8cf6b9614bfe25c0
Gzoref <gzoref@gmail.com>
2019-11-12 14:49:37 -0500

    Append text to file file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+
+/**
+ * append_text_to_file - Appends text at the end of a file
+ *
+ * @filename: Const char pointer
+ *
+ * @text_content: Char pointer
+ *
+ * Return: int
+ */
+
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fp;
+	ssize_t bytes = 0, length = 0;
+
+	while (text_content[length] != '\0')
+	{
+		length++;
+	}
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	fp = open(filename, O_WRONLY | O_APPEND);
+
+	if (fp == -1)
+	{
+		return (-1);
+	}
+
+	bytes = write(fp, text_content, length);
+
+	if (bytes == -1)
+	{
+		return (-1);
+	}
+	close(fp);
+
+	return (1);
+}
