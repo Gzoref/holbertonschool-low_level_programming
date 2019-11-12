1667f88efbda7f61d909eef756c022d0a378f1df
Gzoref <gzoref@gmail.com>
2019-11-12 18:10:42 -0500

    Create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,35 +9,31 @@
  *
  * Return: int
  */
-
 int create_file(const char *filename, char *text_content)
 {
-	int fp;
-	ssize_t length = 0, bytes;
+	int fp, length = 0, fpWrite;
+
+	fp = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+
+	if (fp == -1)
+		return (-1);
+
+	if (text_content == NULL)
+	{
+		text_content = "";
+	}
 
 	while (text_content[length] != '\0')
 	{
 		length++;
 	}
 
-	fp = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fpWrite = write(fp, text_content, length);
 
-	if (fp == -1)
-	{
-		return (-1);
-	}
-
-	if (text_content == NULL)
-	{
-		return (1);
-	}
-	bytes = write(fp, text_content, length);
-
-	if (bytes == -1)
+	if (fpWrite == -1)
 	{
 		return (-1);
 	}
 	close(fp);
-
 	return (1);
 }
