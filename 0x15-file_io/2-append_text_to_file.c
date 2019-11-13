8d4e9321707d35d9115dc6d66c019da8625dbee5
Gzoref <gzorf@gmail.com>
2019-11-13 11:23:25 -0500

    Append to file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -32,7 +32,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		{
 			bytes = write(fp, text_content, length);
 		}
-		close(fp);
+
 		if (bytes == -1)
 		{
 			return (-1);
@@ -40,5 +40,5 @@ int append_text_to_file(const char *filename, char *text_content)
 
 		close(fp);
 
-		return (1);
+		return (bytes);
 }
