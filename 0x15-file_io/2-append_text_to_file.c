66130787351640dcf6e448ad7ae1e62e898859f8
Gzoref <gzorf@gmail.com>
2019-11-13 11:29:40 -0500

    Append to file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,35 +13,32 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fp;
-	ssize_t bytes, length = 0;
+	int fdopen, fdwrite, length = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	fdopen = open(filename, O_WRONLY | O_APPEND);
+
+	if (fdopen == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content == NULL)
+		text_content = "";
 
 	while (text_content[length] != '\0')
 	{
 		length++;
 	}
 
-	if (!filename)
+	fdwrite = write(fdopen, text_content, length);
+
+	if (fdwrite == -1)
 		return (-1);
 
-	if (text_content)
-	{
+	close(fdopen);
 
-	fp = open(filename, O_WRONLY | O_APPEND);
-
-	if (fp == -1)
-	{
-		return (-1);
-	}
-
-	bytes = write(fp, text_content, length);
-
-		if (bytes == -1)
-		{
-			return (-1);
-		}
-
-		close(fp);
-	}
-		return (bytes);
+	return (1);
 }
