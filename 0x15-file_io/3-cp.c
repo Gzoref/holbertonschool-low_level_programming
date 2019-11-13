f4f32a874a9879b1382641c252bde62d337627f8
Gzoref <gzoref@gmail.com>
2019-11-13 18:36:52 -0500

    Copy file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,36 +12,43 @@
 
 int main(int argc, char **argv)
 {
-	int fd, fd2, fdWrite;
-	ssize_t fdRead;
+	int fd, fd2, filecheck;
 	char buffer[1024];
 
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-
 	fd = open(argv[1], O_RDONLY);
-
 	if (fd == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	fd2 = open(argv[2], (O_CREAT | O_WRONLY | O_TRUNC), 0664);
+	fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd2 == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to  %s\n", argv[2]), exit(99);
-
-	while ((fdRead = read(fd, buffer, sizeof(buffer) != 0)))
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	while ((filecheck = read(fd, buffer, 1024)) > 0)
 	{
-		if (fdRead == 1)
+		if (filecheck == -1)
+		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-		fdWrite =  write(fd, buffer, fdRead);
-		if (fdWrite == -1)
-			dprintf(STDERR_FILENO, "Error: Can't write to  %s\n", argv[2]), exit(99);
+			exit(98);
+		}
+		filecheck = write(fd2, buffer, filecheck);
+		if (filecheck == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
 	}
-	if (close(fd == -1))
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd), exit(100);
-	if (close(fd2 == -1))
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2), exit(100);
+	if (filecheck == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (close(fd) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file1), exit(100);
+	if (close(fd2) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file2), exit(100);
+
 	return (0);
 }
