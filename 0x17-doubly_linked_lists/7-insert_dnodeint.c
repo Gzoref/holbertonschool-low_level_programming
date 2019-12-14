908084285679eb2c99378250c73dce0dd4a96b30
Gzoref <gzoref@gmail.com>
2019-12-14 15:20:23 -0500

    Add node to indexed position

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,8 +14,9 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+	dlistint_t *current;
 	unsigned int count = 0;
-	dlistint_t *current, *new_node = malloc(sizeof(dlistint_t));
 
 	if (h == NULL || new_node == NULL)
 	{
@@ -23,20 +24,26 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	new_node->n = n;
 	new_node->next = NULL;
-
-	if (!idx)
-	{
-		new_node->next = *h;
-		*h = new_node;
-		return (new_node);
-	}
+	new_node->prev = NULL;
 	current = *h;
 
+	if (idx == 0)
+	{
+		new_node = add_dnodeint(h, n);
+		return (new_node);
+	}
 	while (current)
 	{
-		if (count == (idx - 1))
+		if (current->next == NULL && count == idx - 1)
+		{
+			new_node = add_dnodeint_end(h, n);
+			return (new_node);
+		}
+		else if ((idx - 1) == count)
 		{
 			new_node->next = current->next;
+			new_node->prev = current;
+			current->next->prev = new_node;
 			current->next = new_node;
 			return (new_node);
 		}
@@ -44,6 +51,5 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		current = current->next;
 	}
 	free(new_node);
-
 	return (NULL);
 }
