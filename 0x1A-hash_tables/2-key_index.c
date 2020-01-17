586656df70ce3f20bb9fba26e099ca0b22c9e8f3
Gzoref <gzoref@gmail.com>
2020-01-17 13:12:05 -0500

    Add 2-key_index.c

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+
+/**
+ *  key_index - Creates a hash table
+ *
+ *  @key: Key to hashtable
+ *
+ *  @size: Size of the array
+ *
+ *  Return: A pointer to the newly created hash table
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	const unsigned long int djb2 = hash_djb2(key) / size;
+
+	return (djb2);
+}
