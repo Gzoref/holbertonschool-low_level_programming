4f20cdfd2cff7d2b489072b929e5652a515798d9
Gzoref <gzoref@gmail.com>
2020-01-18 12:58:13 -0500

    Valgrind fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,8 +10,6 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	unsigned long int index;
-
 	hash_table_t *create_table = NULL;
 
 	create_table = malloc(sizeof(hash_table_t));
@@ -29,11 +27,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 	{
 		free(create_table);
 		return (NULL);
-	}
-
-	for (index = 0; index < size; index++)
-	{
-		create_table->array[index] = NULL;
 	}
 
 	return (create_table);
