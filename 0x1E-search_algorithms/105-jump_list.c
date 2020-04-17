f0d2f137382da2e7ab3909061ec42c90847b6fbe
Gzoref <gzoref@gmail.com>
2020-04-17 17:36:31 -0400

    jump_list file

---
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -0,0 +1,18 @@
+#include "search_algos.h"
+#include <math.h>
+
+/**
+ * jump_list - Searches value in a linked list with the Jump search
+ *
+ * @list: list to search
+ *
+ * @size: Size of the array
+ *
+ * @value: Value to search
+ *
+ * Return: First index where value is located or -1 for NULL array
+ */
+
+listint_t *jump_list(listint_t *list, size_t size, int value)
+{
+}
