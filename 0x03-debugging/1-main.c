abbee29f6768da740ba5a4c4b78402711f1911b1
Gzoref <gzoref@gmail.com>
2020-09-16 23:22:35 -0400

    Create main files

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
+
 /**
- * main - causes an infinite loop
- * Return: 0
- */
+* main - causes an infinite loop
+* Return: 0
+*/
+
 int main(void)
 {
-/*
- * int i;
- */
-	printf("Infinite loop incoming :(\n");
-/*
- * i = 0;
- */
-/*
- * while (i < 10)
- * {
- * putchar(i);
- * }
- */
-	printf("Infinite loop avoided! \\o/\n");
+        int i;
 
-	return (0);
+        printf("Infinite loop incoming :(\n");
+
+        i = 0;
+
+        while (i < 10)
+        {
+                putchar(i);
+        }
+
+        printf("Infinite loop avoided! \\o/\n");
+
+        return (0);
 }
